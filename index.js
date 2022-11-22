8da6b39c516bf96a149d6c186e6609b5701e9dd2
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-11-23 00:14:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var a = new Audio('./sounds/a.mp3')
+var d = new Audio('./sounds/d.mp3')
+var s = new Audio('./sounds/s.mp3')
+var j = new Audio('./sounds/j.mp3')
+var k = new Audio('./sounds/k.mp3')
+var l = new Audio('./sounds/l.mp3')
+var w = new Audio('./sounds/w.mp3')
+
+
+document.querySelectorAll('button')[0].addEventListener('click', function() {
+    a.play();
+})
+
+document.querySelectorAll('button')[1].addEventListener('click', function() {
+    w.play();
+})
+
+document.querySelectorAll('button')[2].addEventListener('click', function() {
+    s.play();
+})
+
+document.querySelectorAll('button')[3].addEventListener('click', function() {
+    d.play();
+})
+
+document.querySelectorAll('button')[4].addEventListener('click', function() {
+    j.play();
+})
+
+document.querySelectorAll('button')[5].addEventListener('click', function() {
+    k.play();
+})
+
+document.querySelectorAll('button')[6].addEventListener('click', function() {
+    l.play();
+})
